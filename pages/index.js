f7b2e5a75372ea19b264e12046dcad2c3c1a5429
Danilo Pádua <dpaduaoliveira@gmail.com>
2021-06-04 17:35:27 -0300

    chore: git initial project quiz react alura

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import Widget from '../src/components/Widget'
+import QuizBackground from '../src/components/QuizBackground'
+import QuizContainer from '../src/components/QuizContainer'
+import Footer from '../src/components/Footer'
+import db from '../db.json'
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer/>
+      </QuizContainer>
+    </QuizBackground>
+  )
 }
---
+++ src/components/QuizContainer/index.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const QuizContainer = styled.div`
+  flex: 1 auto;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 1rem;
+  }
+`
+export default QuizContainer
